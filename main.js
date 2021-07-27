c7f18db91b2f572e41007424d90d8d954c1273af
Emily Straka <81768521+emstraka@users.noreply.github.com>
2021-07-27 19:35:03 -0400

    worked on js function

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-const emailEl = document.querySelector('#email');
-const form = document.querySelector('#send');
+function validateForm(){
+    const userEmail = document.getElementById('userEmail').value;
+    const userMsg = document.getElementById('message').value;
 
-const isEmailValid = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-};
+    const validEmail = userEmail.includes('@');
+    const validMsg = userMsg.length > 1;
 
-const showError = (input, message) => {
-    // get the form-field element
-    const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
-
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-
-const checkEmail = () => {
-    let valid = false;
-    const email = emailEl.value.trim();
-    if (!isRequired(email)) {
-        showError(emailEl, 'Email cannot be blank.');
-    } else if (!isEmailValid(email)) {
-        showError(emailEl, 'Email is not valid.')
+    if (validEmail && validMsg) {
+        alert('Thank you for contacting me!')
+        return true
     } else {
-        showSuccess(emailEl);
-        valid = true;
+        alert('Please enter a valid email address and message.')
+        return false
     }
-    return valid;
 }
